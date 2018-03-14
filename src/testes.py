f2722a2fb426ed2243dc433692bcc53f929f4419
carolinecristinea30 <36683472+carolinecristinea30@users.noreply.github.com>
2018-03-13 21:52:37 -0300

    Update testes.py

--- src/testes.py
+++ src/testes.py
@@ -11,12 +11,12 @@ else:
     for linha in jogo:
         if len(linha) != 3:
             erroInicializar = True
-      else:
-          for elemento in linha:
-              if elemento != '.':
-                 erroInicializar = True
+        else:
+            for elemento in linha:
+                if elemento != '.':
+                    erroInicializar = True
           
 if erroInicializar:
-  sys.exit(1)
+    sys.exit(1)
 else:
-  sys.exit(0)
+    sys.exit(0)
