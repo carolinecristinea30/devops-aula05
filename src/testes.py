06128f6b43d478079f8561a5c687c92def523a1d
aluno <Aluno@salas.aulas>
2018-03-13 22:26:09 -0300

    fixed ident

--- src/testes.py
+++ src/testes.py
@@ -2,7 +2,6 @@ import jogovelha
 import sys
 
 erroInicializar = False
-
 jogo = jogovelha.inicializar()
 
 if len(jogo) != 3:
@@ -20,19 +19,3 @@ if erroInicializar:
 	sys.exit(1)
 else:
 	sys.exit(0)
-=======
-    erroInicializar = True
-else:
-    for linha in jogo:
-        if len(linha) != 3:
-            erroInicializar = True
-        else:
-            for elemento in linha:
-                if elemento != '.':
-                    erroInicializar = True
-          
-if erroInicializar:
-    sys.exit(1)
-else:
-    sys.exit(0)
->>>>>>> f2722a2fb426ed2243dc433692bcc53f929f4419
